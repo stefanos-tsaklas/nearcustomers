17de62a63bd5a3331220b37ab8db86cb2578a001
Stefanos Tsaklas <stsaklas@microsoft.com>
2018-06-23 20:01:31 +0200

    Added data-driven tests for distance calculation

--- test/unit-tests/distance-calculator-impl.spec.ts
+++ test/unit-tests/distance-calculator-impl.spec.ts
@@ -7,28 +7,69 @@ chai.use(chaiSpies);
 
 const expect = chai.expect;
 
+//latitude: [-90, 0, +90] longitude: [-180, 0, 180 ]
 describe('DistanceCalculatorImpl test suite', () => {
     const locations = {
-        "prague":       { longitude: 14.4378005, latitude: 50.0755381 },
-        "california":   { longitude: -119.4179324, latitude: 36.778261 }
+        "prague":           { latitude: +50.0755381, longitude:  +14.4378005 },
+        "brasil":           { latitude: -23.5337730, longitude:  -46.6252900 },
+        "california":       { latitude: +36.7782610, longitude: -119.4179324 },
+        "south_africa":     { latitude: -26.1952460, longitude:  +28.0340880 },
+        "dublin":           { latitude: +53.3394280, longitude:   -6.2576640 },
+        "point_(-90,-180)": { latitude: -90.0000000, longitude: -180.0000000 },
+        "point_(-90,0)":    { latitude: -90.0000000, longitude:    0.0000000 },
+        "point_(-90,+180)": { latitude: -90.0000000, longitude: +180.0000000 },
+        "point_(0,-180)":   { latitude:   0.0000000, longitude: -180.0000000 },
+        "point_(0,0)":      { latitude:   0.0000000, longitude:    0.0000000 },
+        "point_(0,+180)":   { latitude:   0.0000000, longitude: +180.0000000 },
+        "point_(+90,-180)": { latitude: +90.0000000, longitude: -180.0000000 },
+        "point_(+90,0)":    { latitude: +90.0000000, longitude:    0.0000000 },
+        "point_(+90,+180)": { latitude: +90.0000000, longitude: +180.0000000 },
     };
-
-    const tests = [
-        { args: [ locations.prague, locations.prague ], expected: 0 },
-        { args: [ locations.prague, locations.california ], expected: 9349.68 },
-        { args: [ locations.california, locations.prague ], expected: 9349.68 },
+    
+    // expected distances calculated by https://gps-coordinates.org/distance-between-coordinates.php
+    const citiesTests = [
+        { point1: locations.prague, point2: locations.prague, expected: 0 },
+        { point1: locations.prague, point2: locations.california, expected: 9349.68 },
+        { point1: locations.prague, point2: locations.brasil, expected: 10144.64 },
+        { point1: locations.california, point2: locations.prague, expected: 9349.68 },
+        { point1: locations.california, point2: locations.brasil, expected: 10146.59 },
+    ];
+    const extremeTests = [
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(+90,-180)"], expected: 20015.09 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(+90,0)"], expected: 20015.09 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(+90,+180)"], expected: 20015.09 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(-90,-180)"], expected: 0 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(-90,0)"], expected: 0 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(-90,+180)"], expected: 0 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(0,-180)"], expected: 10007.54 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(0,0)"], expected: 10007.54 },
+        { point1: locations["point_(-90,-180)"], point2: locations["point_(0,+180)"], expected: 10007.54 },
     ];
 
-    tests.forEach(function(test) {
-        it(`correctly calculates distance between ${JSON.stringify(test.args[0])} and ${JSON.stringify(test.args[1])} as ${test.expected}`, function() {
+    citiesTests.forEach(function(test) {
+        it(`correctly calculates distance between cities at ${JSON.stringify(test.point1)} and ${JSON.stringify(test.point2)} as ${test.expected}`, function() {
 
             // arrange
             let calculator = new DistanceCalculatorImpl();
 
             // act
-            let result = calculator.GetDistanceInKilometers(test.args[0], test.args[1]);
+            let result = calculator.GetDistanceInKilometers(test.point1, test.point2);
 
-            // assert
+            // assert distance with tolerance of 1 kilometer
+            expect(result).to.be.closeTo(test.expected, 1);
+        });
+    });
+
+    extremeTests.forEach(function(test) {
+        it(`correctly calculates distance between cities at ${JSON.stringify(test.point1)} and ${JSON.stringify(test.point2)} as ${test.expected}`, function() {
+
+            // arrange
+            let calculator = new DistanceCalculatorImpl();
+
+            // act
+            let result = calculator.GetDistanceInKilometers(test.point1, test.point2);
+
+            // assert distance with tolerance of 1 kilometer
             expect(result).to.be.closeTo(test.expected, 1);
         });
     });
